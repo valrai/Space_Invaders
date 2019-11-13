924ec107075c69b98779b341a1cd2da7589685f0
Valdecir <rm.valdecir@gmail.com>
2019-11-13 01:07:34 -0300

    Adding menu, fixing some bugs

--- main.cpp
+++ main.cpp
@@ -28,14 +28,15 @@ typedef struct // criando struct que representará as naves
   {
     int Col;
     int Lin;
+
   }Shot;
 
 }SpaceShip;
 
+bool StageisCompleted; // indica se o jogador já derrotou todos os inimigos
 SpaceShip Player; // criando a nave do jogador
 SpaceShip Enemies[AmntEnemies]; // criando um array de naves inimigas
 std::mt19937 generator; //instanciando gerador de números aleatórios
-bool StageisCompleted; // indica se o jogador já derrotou todos os inimigos
 
 void setcursor(int x, int y);
 void PrintFrame();
@@ -49,6 +50,9 @@ void Player_Shot();
 void Player_Movimentation(char Key);
 void DefeatAnimation();
 void VictoryAnimation();
+void MainMenu();
+void Inicialize();
+bool PlayerIsDead();
 
 // ===================================== Set Cursor Position ==========================================
 
@@ -157,7 +161,7 @@ void Player_Shot() // realiza a animação do tiro da nave do jogador
   }
 }
 
-// ================================== Shot Hit the Target ================================================
+// ================================== Shot Hit some Enemy ================================================
 
 void Shot_Hit_Enemy()  // verifica se o tiro atingiu anlguma nave inimiga
 {
@@ -228,31 +232,33 @@ int Get_Amount_of_Enemies_Killed()
 
 // ================================== Choose Enemy to Shoot ====================================================
 
-void Choose_Enemy_to_Shoot()
+int Choose_Enemy_to_Shoot()
 {
   int Index = 0, ShotCount = 0;
-  bool StageClean;
+  bool IsOver;
 
   do{
     Sleep(2000);
     ShotCount = 0;
-    StageClean = Get_Amount_of_Enemies_Killed() == AmntEnemies;
+    StageisCompleted = Get_Amount_of_Enemies_Killed() == AmntEnemies;
 
     do{
       Index = GetRandomNumber(0, AmntEnemies-1);
 
-      if(!Enemies[Index].Is_Shoting && Enemies[Index].Is_Alive)
+      if(Enemies[Index].Is_Shoting == false && Enemies[Index].Is_Alive)
       {
         Enemies[Index].Is_Shoting = true;
         Enemies[Index].Shot.Col = Enemies[Index].Col + 5;
         ShotCount++;
       }
-    }while(ShotCount <= 2 && StageClean == false);
 
-  } while (StageClean == false);
+      IsOver = StageisCompleted || Player.Is_Alive == false;
 
-  StageisCompleted = true;
+    }while(ShotCount <= 2 && IsOver == false);
 
+  } while (IsOver == false);
+
+  return 0;
 }
 
 // ================================== Enemy Shot ==============================================================
@@ -318,7 +324,7 @@ void PlayerExplosion() // realiza a animação da explosão das naves inimigas
 
 // ================================== Shot Hit the Player ===============================================
 
-bool Shot_Hit_Player()  // verifica se o tiro atingiu anlguma nave inimiga
+bool PlayerIsDead()  // verifica se o jogador perdeu
 {
   for (int i = 0; i < AmntEnemies; ++i) // percorre o array de inimigos
   {
@@ -326,8 +332,8 @@ bool Shot_Hit_Player()  // verifica se o tiro atingiu anlguma nave inimiga
       for (int j = 0; j < 8; ++j) // percorre todas as 10 colunas que constituem a nave inimiga
         if (Enemies[i].Shot.Col == (Player.Col + j)) // e estiver na mesma coluna que a nave inimiga (que deve estar "viva")...
         {
-          textcolor(0)
-;          setcursor(Enemies[i].Shot.Col, Enemies[i].Shot.Lin + 1);printf(" "); // remove da tela o tiro
+          textcolor(0);
+          setcursor(Enemies[i].Shot.Col, Enemies[i].Shot.Lin + 1);printf(" "); // remove da tela o tiro
           PlayerExplosion(); // executa a animação de explosão
           Player.Shot.Lin = Player.Lin - 1;
           Enemies[i].Is_Shoting = false;
@@ -344,6 +350,10 @@ void Main_Stream()
 {
   bool toRight = true, Ready;
   unsigned char key = 0;
+
+  system("CLEAR || CLS");
+  PrintFrame();
+  Print_Player_Spaceship();
 
   auto future = std::async(std::launch::async, GetKey);
   auto f = std::async(std::launch::async, Choose_Enemy_to_Shoot);
@@ -396,16 +406,14 @@ void Main_Stream()
 
     for (int i = 0; i < AmntEnemies; ++i)
       if(Enemies[i].Is_Shoting)
-         std::async(std::launch::async, EnemyShot, i);
+        EnemyShot(i);
 
-  }while(key != 27 && Shot_Hit_Player() == false && StageisCompleted == false);
+  }while(key != VK_ESCAPE && PlayerIsDead() == false && StageisCompleted == false);
 
   if(StageisCompleted)
     VictoryAnimation();
-  else if(Shot_Hit_Player)
+  else if(Player.Is_Alive == false)
     DefeatAnimation();
-  //else
-    //MainMenu();
 
 }
 
@@ -454,7 +462,7 @@ void DefeatAnimation()
   system("clear||cls");
   PrintFrame();
 
-  for(int i = 0; i<30; i++)
+  for(int i = 0; i < 15; i++)
   {
     textcolor(Colors[Cont]);
     setcursor(Col, Lin);  printf(" __     __                                   _______                          __                      __ ");
@@ -476,6 +484,7 @@ void DefeatAnimation()
     setcursor(40, 30);
     textcolor(15);
     system("PAUSE");
+    MainMenu();
 }
 
 // ================================== Victory Animation ===========================================
@@ -488,7 +497,7 @@ void VictoryAnimation()
   system("clear||cls");
   PrintFrame();
 
-  for(int i = 0; i<30; i++)
+  for(int i = 0; i < 15; i++)
   {
     textcolor(Colors[Cont]);
     setcursor(Col, Lin); printf(" /$$$$$$$$                                                            ");
@@ -517,26 +526,135 @@ void VictoryAnimation()
     setcursor(40, 30);
     textcolor(15);
     system("PAUSE");
+    MainMenu();
 }
 
-// ================================= Main =========================================================
+// ================================= Print Menu Options =====================================================
 
-int main()
+void Print_Menu_Options(char PreviousIndex, char arrowIndex)
 {
-  system("MODE con cols=138 lines=38"); // define o tamanho do terminal
-  _setcursortype(_NOCURSOR); // deixa o cursor invisível
+  char Arrow = arrowIndex * 2 + 20;
+  char WheretoClean = PreviousIndex * 2 + 20;
 
+  textcolor(0);
+  setcursor(50, WheretoClean); printf("  ");
+
+  textcolor(9);
+  setcursor(50, Arrow); printf("|>");
+  textcolor(2);
+  setcursor(53,22); printf(" JOGAR");
+  setcursor(53,24); printf(" CONTROLES");
+  setcursor(53,26); printf(" INSTRUCOES");
+  setcursor(53,28); printf(" SAIR");
+  textcolor(3);
+  setcursor(43,34); printf("Produzido por Valdecir Raimundo");
+}
+
+// ================================= Main Menu =====================================================
+
+void MainMenu()
+{
+  int Lin = 5, Col = 22, Cont = 0, MenuIndex = 1, OptionsAmount = 4;
+  int Colors[] = {3, 13, 9, 14, 7};
+  auto future = std::async(std::launch::async, GetKey);
+  char Key;
+
+  system("CLEAR || CLS");
+  Inicialize();
+  PrintFrame();
+  Print_Menu_Options(OptionsAmount, MenuIndex);
+
+  do{
+      textcolor(Colors[Cont]);
+      setcursor(Col, Lin); printf("     _______..______      ___       ______  _______                              ");
+      setcursor(Col, Lin + 1); printf("    /       ||   _  \\    /   \\     /      ||   ____|                              ");
+      setcursor(Col, Lin + 2); printf("   |   (----`|  |_)  |  /  ^  \\   |  ,----'|  |__                                 ");
+      setcursor(Col, Lin + 3); printf("    \\   \\    |   ___/  /  /_\\  \\  |  |     |   __|                                ");
+      setcursor(Col, Lin + 4); printf(".----)   |   |  |     /  _____  \\ |  `----.|  |____                               ");
+      setcursor(Col, Lin + 5); printf("|____.__/  __. ____  /_____  ____\\ \\____________________ .______          _______.");
+      setcursor(Col, Lin + 6); printf("|  | |  \\ |  | \\   \\  /   / /   \\     |       \\ |   ____||   _  \\        /       |");
+      setcursor(Col, Lin + 7); printf("|  | |   \\|  |  \\   \\/   / /  ^  \\    |  .--.  ||  |__   |  |_)  |      |   (----`");
+      setcursor(Col, Lin + 8); printf("|  | |  . `  |   \\      / /  /_\\  \\   |  |  |  ||   __|  |      /        \\   \\    ");
+      setcursor(Col, Lin + 9); printf("|  | |  |\\   |    \\    / /  _____  \\  |  '--'  ||  |____ |  |\\  \\----.----)   |   ");
+      setcursor(Col, Lin + 10); printf("|__| |__| \\__|     \\__/ /__/     \\__\\ |_______/ |_______|| _| `._____|_______/    ");
+
+      Cont++;
+      if (Cont == OptionsAmount)
+        Cont = 0;    
+
+    if (future.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+    {
+      Key = future.get();
+
+      if (Key == 'w' || Key == 'W')
+      {
+        if(MenuIndex == 1 )
+        {
+          MenuIndex = OptionsAmount;
+          Print_Menu_Options(1, MenuIndex);
+        }
+        else
+        {
+          MenuIndex--;
+          Print_Menu_Options(MenuIndex + 1, MenuIndex);
+        }
+      }
+      else if (Key == 's' || Key == 'S' )
+      {
+        if(MenuIndex == OptionsAmount )
+        {
+          MenuIndex = 1;
+          Print_Menu_Options(OptionsAmount, MenuIndex);
+        }
+        else
+        {
+          MenuIndex++;
+          Print_Menu_Options(MenuIndex - 1, MenuIndex);
+        }
+      }
+      else if( Key == 13)
+      {
+        switch (MenuIndex)
+        {
+          case 1:
+            Main_Stream();
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          case 4:
+            Key = VK_ESCAPE;
+            break;
+        }
+      }
+
+      future = std::async(std::launch::async, GetKey);
+    }
+    Sleep(200);
+
+  }while(Key != VK_ESCAPE);
+
+}
+
+// ================================= Inicialize ==================================================
+
+void Inicialize()
+{
+  Player.Is_Alive = true;
+  Player.Is_Shoting = false;
   Player.Col = (FrameCol - 6)/2 - 4; // inicializando as posições da nave do jogador
   Player.Lin = FrameLin - 6;
   Player.Shot.Lin = Player.Lin - 1;
+  StageisCompleted = false;
 
   Enemies[0].Col = 19;
   for (int i = 0; i < AmntEnemies; ++i) // inicializando as posições das naves inimigas
   {
-    if (i<6)
+    if (i < 6)
     {
       Enemies[i].Lin = 3;
-      if(i>0)
+      if(i > 0)
         Enemies[i].Col = Enemies[i-1].Col + 14;
     }
     else
@@ -545,11 +663,20 @@ int main()
       Enemies[i].Col = Enemies[i-6].Col;
     }
     Enemies[i].Shot.Lin = 14;
+    Enemies[i].Is_Shoting = false;
+    Enemies[i].Is_Alive = true;
   }
+}
+
+// ================================= Main =========================================================
+
+int main()
+{
+  system("MODE con cols=138 lines=38"); // define o tamanho do terminal
+  _setcursortype(_NOCURSOR); // deixa o cursor invisível
   generator.seed((unsigned int)std::time(0)); //utilizando o valor retornado pela função time(0) para inicializar o gerador de números aleatórios.
-  PrintFrame();
-  Print_Player_Spaceship();
-  Main_Stream();
+
+  MainMenu();
 
   return 0;
 }
